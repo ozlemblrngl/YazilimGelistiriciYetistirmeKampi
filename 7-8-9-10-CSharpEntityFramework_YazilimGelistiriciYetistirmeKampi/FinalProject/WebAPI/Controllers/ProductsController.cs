1798e086869dd828b0a2af8fa635bd5bd3766b29
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2023-11-18 00:03:07 +0300

    added correct

---
+++ 7-8-9-10-CSharpEntityFramework_YazilimGelistiriciYetistirmeKampi/FinalProject/WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")] //--------> bu insanlar bize nasıl ulaşsın? başına api yazsın sonra controllerı yazsın örn api/products
+    [ApiController] // ------------> Attribute // Java'da da Annotation denir.
+    public class ProductsController : ControllerBase //------> controllerın adı Products'tır.
+    {
+     
+      // bir controller'ın controller olabilmesi için onun ControllerBase'den inherit etmesi lazım.
+      // ve de [ApiController] attribute'u taşıması lazım.
+      // Attribute bir class ile ilgili bilgi verme ve onu imzalama yöntemidir.
+      // kısaca burada diyoruz ki ProductController sınıfı bir controller'dır.
+
+      [HttpGet]
+      public string Get()
+      {
+          return "merhaba";
+      }
+
+        
+    }
+}
