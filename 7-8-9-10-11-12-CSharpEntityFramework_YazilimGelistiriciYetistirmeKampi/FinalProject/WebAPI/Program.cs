a9610c72c1e123546017d42d1d4692f684adf650
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2023-12-22 23:23:02 +0300

    trash codes deleted

--- 7-8-9-10-11-12-CSharpEntityFramework_YazilimGelistiriciYetistirmeKampi/FinalProject/WebAPI/Program.cs
+++ 7-8-9-10-11-12-CSharpEntityFramework_YazilimGelistiriciYetistirmeKampi/FinalProject/WebAPI/Program.cs
@@ -109,27 +109,6 @@ public class Program
 
         app.Run();
 
-
-
-
-        //// ASP.NET Core 3.0+:
-        //// The UseServiceProviderFactory call attaches the
-        //// Autofac provider to the generic hosting mechanism.
-        //var host = Host.CreateDefaultBuilder(args)
-        //    .UseServiceProviderFactory(new AutofacServiceProviderFactory())
-        //    .ConfigureContainer<ContainerBuilder>(builder =>
-        //    {
-        //        builder.RegisterModule(new AutofacBusinessModule());
-        //    })
-        //    .ConfigureWebHostDefaults(webHostBuilder => {
-        //        webHostBuilder
-        //      .UseContentRoot(Directory.GetCurrentDirectory())
-        //        .UseIISIntegration()
-        //      .UseStartup<Program>();
-        //    })
-        //    .Build();
-
-        //host.Run();
     }
 
 
